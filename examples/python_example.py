82ca87eddac7a4ed7fb876f10fa81fda90488839
Beau Gunderson <beau@beaugunderson.com>
2012-06-01 15:29:54 -0700

    Bring DataDog/statsd in line with etsy/statsd
    Extracted (almost) all DataDog-specific code to backends/datadog.js.

--- examples/python_example.py
+++ examples/python_example.py
@@ -5,10 +5,8 @@
  
 # this file expects local_settings.py to be in the same dir, with statsd host and port information:
 # 
-from socket import socket, AF_INET, SOCK_DGRAM
-
-statsd_host = 'localhost'
-statsd_port = 8125
+# statsd_host = 'localhost'
+# statsd_port = 8125
 
 # Sends statistics to the stats daemon over UDP
 class Statsd(object):
@@ -60,9 +58,13 @@ class Statsd(object):
         """
         Squirt the metrics over UDP
         """
-        host = statsd_host
-        port = statsd_port
-        addr=(host, port)
+        try:
+            import local_settings as settings
+            host = settings.statsd_host
+            port = settings.statsd_port
+            addr=(host, port)
+        except Error:
+            exit(1)
         
         sampled_data = {}
         
@@ -70,11 +72,12 @@ class Statsd(object):
             import random
             if random.random() <= sample_rate:
                 for stat in data.keys():
-                    value = data[stat]
+                    value = sampled_data[stat]
                     sampled_data[stat] = "%s|@%s" %(value, sample_rate)
         else:
             sampled_data=data
         
+        from socket import *
         udp_sock = socket(AF_INET, SOCK_DGRAM)
         try:
             for stat in sampled_data.keys():
@@ -86,8 +89,3 @@ class Statsd(object):
             from pprint import pprint
             print "Unexpected error:", pprint(sys.exc_info())
             pass # we don't care
-
-if __name__ == '__main__':
-    Statsd.increment('test.data.counter', 1)
-
-
--- stats.js
+++ stats.js
@@ -3,22 +3,61 @@ var dgram  = require('dgram')
   , net    = require('net')
   , config = require('./config')
   , fs     = require('fs')
+  , events = require('events')
 
 var keyCounter = {};
 var counters = {};
 var timers = {};
-var debugInt, flushInt, keyFlushInt, server, mgmtServer;
+var gauges = {};
+var pctThreshold = null;
+var debugInt, flushInterval, keyFlushInt, server, mgmtServer;
 var startup_time = Math.round(new Date().getTime() / 1000);
+var backendEvents = new events.EventEmitter();
+
+// Load and init the backend from the backends/ directory.
+function loadBackend(config, name) {
+  var backendmod = require(name);
+
+  if (config.debug) {
+    util.log("Loading backend: " + name);
+  }
+
+  var ret = backendmod.init(startup_time, config, backendEvents);
+  if (!ret) {
+    util.log("Failed to load backend: " + name);
+    process.exit(1);
+  }
+};
+
+// Flush metrics to each backend.
+function flushMetrics() {
+  var time_stamp = Math.round(new Date().getTime() / 1000);
+
+  var metrics_hash = {
+    counters: counters,
+    gauges: gauges,
+    timers: timers,
+    pctThreshold: pctThreshold
+  }
+
+  // After all listeners, reset the stats
+  backendEvents.once('flush', function clear_metrics(ts, metrics) {
+    // Clear the counters
+    for (key in metrics.counters) {
+      metrics.counters[key] = 0;
+    }
+
+    // Clear the timers
+    for (key in metrics.timers) {
+      metrics.timers[key] = [];
+    }
+  });
+
+  // Flush metrics to each backend.
+  backendEvents.emit('flush', time_stamp, metrics_hash);
+};
 
 var stats = {
-  graphite: {
-    last_flush: startup_time,
-    last_exception: startup_time
-  },
-  datadog: {
-    last_flush: null,
-    last_exception: null,
-  },
   messages: {
     last_msg_seen: startup_time,
     bad_lines_seen: 0,
@@ -27,22 +66,16 @@ var stats = {
 
 config.configFile(process.argv[2], function (config, oldConfig) {
   if (! config.debug && debugInt) {
-    clearInterval(debugInt); 
+    clearInterval(debugInt);
     debugInt = false;
   }
 
   if (config.debug) {
     if (debugInt !== undefined) { clearInterval(debugInt); }
-    debugInt = setInterval(function () { 
-      util.log("Counters:\n" + util.inspect(counters) + "\nTimers:\n" + util.inspect(timers));
-      if (config.datadogApiKey) {
-        if (stats['datadog']['last_flush']) {
-          util.log("Datadog - last flush: " + stats['datadog']['last_flush']);
-        }
-        if (stats['datadog']['last_exception']) {
-          util.log("Datadog - last exception: " + stats['datadog']['last_exception']);
-        }
-      }
+    debugInt = setInterval(function () {
+      util.log("Counters:\n" + util.inspect(counters) +
+               "\nTimers:\n" + util.inspect(timers) +
+               "\nGauges:\n" + util.inspect(gauges));
     }, config.debugInterval || 10000);
   }
 
@@ -83,6 +116,8 @@ config.configFile(process.argv[2], function (config, oldConfig) {
             timers[key] = [];
           }
           timers[key].push(Number(fields[0] || 0));
+        } else if (fields[1].trim() == "g") {
+          gauges[key] = Number(fields[0] || 0);
         } else {
           if (fields[2] && fields[2].match(/^@([\d\.]+)/)) {
             sampleRate = Number(fields[2].match(/^@([\d\.]+)/)[1]);
@@ -106,7 +141,7 @@ config.configFile(process.argv[2], function (config, oldConfig) {
 
         switch(cmd) {
           case "help":
-            stream.write("Commands: stats, counters, timers, delcounters, deltimers, quit\n\n");
+            stream.write("Commands: stats, counters, timers, gauges, delcounters, deltimers, delgauges, quit\n\n");
             break;
 
           case "stats":
@@ -115,21 +150,40 @@ config.configFile(process.argv[2], function (config, oldConfig) {
 
             stream.write("uptime: " + uptime + "\n");
 
+            var stat_writer = function(group, metric, val) {
+              var delta;
+
+              if (metric.match("^last_")) {
+                delta = now - val;
+              }
+              else {
+                delta = val;
+              }
+
+              stream.write(group + "." + metric + ": " + delta + "\n");
+            };
+
+            // Loop through the base stats
             for (group in stats) {
               for (metric in stats[group]) {
-                var val;
-
-                if (metric.match("^last_")) {
-                  val = now - stats[group][metric];
-                }
-                else {
-                  val = stats[group][metric];
-                }
-
-                stream.write(group + "." + metric + ": " + val + "\n");
+                stat_writer(group, metric, stats[group][metric]);
               }
             }
-            stream.write("END\n\n");
+
+            backendEvents.once('status', function(writeCb) {
+              stream.write("END\n\n");
+            });
+
+            // Let each backend contribute its status
+            backendEvents.emit('status', function(err, name, stat, val) {
+              if (err) {
+                util.log("Failed to read stats for backend " +
+                         name + ": " + err);
+              } else {
+                stat_writer(name, stat, val);
+              }
+            });
+
             break;
 
           case "counters":
@@ -139,6 +193,11 @@ config.configFile(process.argv[2], function (config, oldConfig) {
 
           case "timers":
             stream.write(util.inspect(timers) + "\n");
+            stream.write("END\n\n");
+            break;
+
+          case "gauges":
+            stream.write(util.inspect(gauges) + "\n");
             stream.write("END\n\n");
             break;
 
@@ -153,6 +212,14 @@ config.configFile(process.argv[2], function (config, oldConfig) {
           case "deltimers":
             for (index in cmdline) {
               delete timers[cmdline[index]];
+              stream.write("deleted: " + cmdline[index] + "\n");
+            }
+            stream.write("END\n\n");
+            break;
+
+          case "delgauges":
+            for (index in cmdline) {
+              delete gauges[cmdline[index]];
               stream.write("deleted: " + cmdline[index] + "\n");
             }
             stream.write("END\n\n");
@@ -175,192 +242,25 @@ config.configFile(process.argv[2], function (config, oldConfig) {
 
     util.log("server is up");
 
-    var flushInterval = Number(config.flushInterval || 10000);
-
-    var pctThreshold = config.percentThreshold || 90;
+    pctThreshold = config.percentThreshold || 90;
     if (!Array.isArray(pctThreshold)) {
       pctThreshold = [ pctThreshold ]; // listify percentiles so single values work the same
     }
 
-    flushInt = setInterval(function () {
-      var statString = '';
-      var ts = Math.round(new Date().getTime() / 1000);
-      var numStats = 0;
-      var key;
-      var snapshotCounters = {}
-      var snapshotTimers = {}
+    flushInterval = Number(config.flushInterval || 10000);
+    config.flushInterval = flushInterval;
 
-      for (key in counters) {
-        snapshotCounters[key] = counters[key];
-        counters[key] = 0
+    if (config.backends) {
+      for (var i = 0; i < config.backends.length; i++) {
+        loadBackend(config, config.backends[i]);
       }
+    } else {
+      // The default backend is graphite
+      loadBackend(config, './backends/graphite');
+    }
 
-      for (key in timers) {
-        snapshotTimers[key] = timers[key];
-        timers[key] = [];
-      }
-
-      for (key in snapshotCounters) {
-        var value = snapshotCounters[key];
-        var valuePerSecond = value / (flushInterval / 1000); // calculate "per second" rate
-
-        statString += 'stats.'        + key + ' ' + valuePerSecond + ' ' + ts + "\n";
-        statString += 'stats_counts.' + key + ' ' + value          + ' ' + ts + "\n";
-
-        numStats += 1;
-      }
-
-      for (key in snapshotTimers) {
-        if (snapshotTimers[key].length > 0) {
-          var values = snapshotTimers[key].sort(function (a,b) { return a-b; });
-          var count = values.length;
-          var min = values[0];
-          var max = values[count - 1];
-
-          var mean = min;
-          var maxAtThreshold = max;
-
-          var message = "";
-
-          var key2;
-
-          for (key2 in pctThreshold) {
-            var pct = pctThreshold[key2];
-            if (count > 1) {
-              var thresholdIndex = Math.round(((100 - pct) / 100) * count);
-              var numInThreshold = count - thresholdIndex;
-              var pctValues = values.slice(0, numInThreshold);
-              maxAtThreshold = pctValues[numInThreshold - 1];
-
-              // average the remaining timings
-              var sum = 0;
-              for (var i = 0; i < numInThreshold; i++) {
-                sum += pctValues[i];
-              }
-
-              mean = sum / numInThreshold;
-            }
-
-            var clean_pct = '' + pct;
-            clean_pct.replace('.', '_');
-            message += 'stats.timers.' + key + '.mean_'  + clean_pct + ' ' + mean           + ' ' + ts + "\n";
-            message += 'stats.timers.' + key + '.upper_' + clean_pct + ' ' + maxAtThreshold + ' ' + ts + "\n";
-          }
-
-          message += 'stats.timers.' + key + '.upper ' + max   + ' ' + ts + "\n";
-          message += 'stats.timers.' + key + '.lower ' + min   + ' ' + ts + "\n";
-          message += 'stats.timers.' + key + '.count ' + count + ' ' + ts + "\n";
-          statString += message;
-
-          numStats += 1;
-        }
-      }
-
-      statString += 'statsd.numStats ' + numStats + ' ' + ts + "\n";
-      
-      // Graphite
-      if (config.graphiteHost) {
-        try {
-          var graphite = net.createConnection(config.graphitePort, config.graphiteHost);
-          graphite.addListener('error', function(connectionException){
-            if (config.debug) {
-              util.log(connectionException);
-            }
-          });
-          graphite.on('connect', function() {
-            this.write(statString);
-            this.end();
-            stats['graphite']['last_flush'] = Math.round(new Date().getTime() / 1000);
-          });
-        } catch(e){
-          if (config.debug) {
-            util.log(e);
-          }
-          stats['graphite']['last_exception'] = Math.round(new Date().getTime() / 1000);
-        }
-      }
-
-      // Datadog
-      if (config.datadogApiKey) {
-          var now = parseInt(new Date().getTime() / 1000);
-          var host = config.hostname || os.hostname();
-          var payload = [];
-
-	  // Send counters
-          for (key in snapshotCounters) {
-            var value = snapshotCounters[key];
-            payload.push({
-              metric: key,
-              points: [[now, value]],
-              type: "counter",
-              host: host
-            })
-          }
-
-	  // Compute timers and send
-          for (key in snapshotTimers) {
-            if (snapshotTimers[key].length > 0) {
-              var pctThreshold = config.percentThreshold || 90;
-              var values = snapshotTimers[key].sort(function (a,b) { return a-b; });
-              var count = values.length;
-              var min = values[0];
-              var max = values[count - 1];
-
-              var mean = min;
-              var maxAtThreshold = max;
-
-              if (count > 1) {
-                var thresholdIndex = Math.round(((100 - pctThreshold) / 100) * count);
-                var numInThreshold = count - thresholdIndex;
-                values = values.slice(0, numInThreshold);
-                maxAtThreshold = values[numInThreshold - 1];
-
-                // average the remaining timings
-                var sum = 0;
-                for (var i = 0; i < numInThreshold; i++) {
-                  sum += values[i];
-                }
-
-                mean = sum / numInThreshold;
-              }
-
-              payload.push({
-                metric: key + '.mean',
-                points: [[now, mean]],
-                type: 'gauge',
-                host: host
-              })
-              payload.push({
-                metric: key + '.upper',
-                points: [[now, max]],
-                type: 'gauge',
-                host: host
-              })
-              payload.push({
-                metric: key + '.upper_' + pctThreshold,
-                points: [[now, maxAtThreshold]],
-                type: 'gauge',
-                host: host
-              })
-              payload.push({
-                metric: key + '.lower',
-                points: [[now, min]],
-                type: 'gauge',
-                host: host
-              })
-              payload.push({
-                metric: key + '.count',
-                points: [[now, count]],
-                type: 'gauge',
-                host: host
-              })
-            }
-          }
-
-          new Datadog(config.datadogApiKey, {api_host: config.datadogApiHost}).metrics(payload);
-      }
-
-    }, flushInterval);
+    // Setup the flush timer
+    var flushInt = setInterval(flushMetrics, flushInterval);
 
     if (keyFlushInterval > 0) {
       var keyFlushPercent = Number((config.keyFlush && config.keyFlush.percent) || 100);
@@ -381,7 +281,7 @@ config.configFile(process.argv[2], function (config, oldConfig) {
 
         // only show the top "keyFlushPercent" keys
         for (var i = 0, e = sortedKeys.length * (keyFlushPercent / 100); i < e; i++) {
-          logMessage += timeString + " " + sortedKeys[i][1] + " " + sortedKeys[i][0] + "\n";
+          logMessage += timeString + " count=" + sortedKeys[i][1] + " key=" + sortedKeys[i][0] + "\n";
         }
 
         var logFile = fs.createWriteStream(keyFlushLog, {flags: 'a+'});
@@ -392,76 +292,5 @@ config.configFile(process.argv[2], function (config, oldConfig) {
         keyCounter = {};
       }, keyFlushInterval);
     }
-
   }
 });
-
-var http = require('http'),
-    https = require('https'),
-    url = require('url'),
-    os = require('os');
-
-function now() {
-    return new Date().getTime()/1000;
-}
-
-var Datadog = function(api_key, options) {
-    var options = options || {};
-
-    this.api_key = api_key;
-
-    this.api_host = options.api_host || 'https://app.datadoghq.com';
-    this.host_name = options.host_name || os.hostname();
-
-    this.pending_requests = 0;
-}
-
-Datadog.prototype.metrics = function(payload) {
-    var client = this;
-    var message = {
-        series: payload
-    };
-    client._post('series', message);    
-}
-
-Datadog.prototype._post = function(controller, message) {
-    var client = this;
-    var body = JSON.stringify(message);
-
-    var transport, transport;
-    var parsed = url.parse(this.api_host);
-    if (parsed.protocol == 'http:') {
-        transport = http;
-        api_port = parsed.port || 80;
-        util.log("Warning! You are about to send unencrypted metrics.");
-    } else {
-        transport = https;
-        api_port = parsed.port || 443;
-    }
-    api_host = parsed.hostname;
-
-    var req = transport.request({
-        host: api_host,
-        port: api_port,
-        path: '/api/v1/' + controller + '?api_key=' + client.api_key,
-        method: 'POST',
-        headers: {
-            "Host": client.api_host,
-            "Content-Length": body.length,
-            "Content-Type": "application/json"
-        }
-    },
-    function(response) {
-        stats['datadog']['last_flush'] = Math.round(new Date().getTime() / 1000);
-        client.pending_requests -= 1;
-    });    
-    req.on('error', function(e) {
-      util.log('Skipping, cannot send data to Datadog: ' + e.message);
-      stats['datadog']['last_exception'] = Math.round(new Date().getTime() / 1000);
-      client.pending_requests -= 1;
-     });
-
-    client.pending_requests += 1;
-    req.write(body);
-    req.end();
-}
